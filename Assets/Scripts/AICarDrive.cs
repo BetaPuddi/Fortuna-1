14095546b7080fbf749a072c5cc75b533fe88526
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2023-11-08 17:20:29 +0000

    Keenan: Started working on car AI
    Started working on car AI.
    The car only moves forwards.

---
+++ Assets/Scripts/AICarDrive.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AICarDrive : MonoBehaviour
+{
+    private float steerAngle;
+    private float currentBreakForce;
+
+    [SerializeField] public float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [SerializeField] private Transform rearRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform frontLeftWheelTransform;
+
+    private InputActionAsset inputActionAsset;
+
+    private InputAction moveAction;
+    private InputAction brakeAction;
+
+    private void FixedUpdate()
+    {
+        HandleMotor();
+        UpdateWheels();
+    }
+
+    private void HandleMotor()
+    {
+        float motorTorque = motorForce;
+
+        frontLeftWheelCollider.motorTorque = motorTorque;
+        frontRightWheelCollider.motorTorque = motorTorque;
+
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        frontRightWheelCollider.brakeTorque = currentBreakForce;
+        frontLeftWheelCollider.brakeTorque = currentBreakForce;
+        rearRightWheelCollider.brakeTorque = currentBreakForce;
+        rearLeftWheelCollider.brakeTorque = currentBreakForce;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
