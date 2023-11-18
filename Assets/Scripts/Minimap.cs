896f5ac6484ca46c28a13a6451fa1b5a9c350a8e
mattez1 <133816825+mattez1@users.noreply.github.com>
2023-11-18 18:21:44 +0000

    x
    x

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+    private void LateUpdate()
+    {
+        Vector3 newpos = player.position;
+        newpos.y = transform.position.y;
+        transform.position = newpos;
+    }
+}
