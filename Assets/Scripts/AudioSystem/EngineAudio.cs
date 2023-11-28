2ddbc2dd84e8941fd78a246d9c16247a22f7dcdf
Reece <wraith0@hotmail.co.uk>
2023-11-28 21:22:41 +0000

    Engine Sound
    Pitch scales relative to velocity

---
+++ Assets/Scripts/AudioSystem/EngineAudio.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace AudioSystem
+{
+    public class EngineAudio : MonoBehaviour
+    {
+        public Rigidbody vehicleRigidbody;
+        public AudioSource engineAudioSource;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            engineAudioSource.pitch = 0.5f + vehicleRigidbody.velocity.magnitude / 100;
+        }
+    }
+}
