4f752331b161fa15a9945257583edfa3eb36d399
mattez1 <133816825+mattez1@users.noreply.github.com>
2023-11-29 21:47:34 +0000

    PositionTracking
    added on keenans "test scene" position tracking , very easy to move between scenes

--- Assets/Scripts/CartLap.cs
+++ Assets/Scripts/CartLap.cs
@@ -1,14 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-//lap number and checkpoint on start
+
+
 public class CartLap : MonoBehaviour
 {
     public int lapNumber;
     public int Checkpoint;
+    [SerializeField]
+    private int position; 
+
+    public int Position 
+    {
+        get { return position; }
+    }
+
+    public void UpdatePosition(int newPosition)
+    {
+        position = newPosition; // Update the position value
+    }
+
     private void Start()
     {
         lapNumber = 1;
         Checkpoint = 0;
+        position = 1; // Initialize position to 1 at the start
     }
 }
