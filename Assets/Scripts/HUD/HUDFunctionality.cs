35351006349dead9b6a930fdef4f5119225624ce
Reece <wraith0@hotmail.co.uk>
2023-12-02 01:34:25 +0000

    HUD Functionality

--- Assets/Scripts/HUD/HUDFunctionality.cs
+++ Assets/Scripts/HUD/HUDFunctionality.cs
@@ -1,23 +1,30 @@
+using PowerupSystem;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 namespace HUD
 {
     public class HUDFunctionality : MonoBehaviour
     {
-        public TextMeshProUGUI positionText;
+        public TextMeshProUGUI positionText, lapText;
+        public Sprite[] powerupIcons;
+        public Image currentPowerupIcon = null;
         public CartLap player;
+        public PowerupContainer playerPowerupContainer;
 
         // Start is called before the first frame update
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
             SetPositionText();
+            SetLapText();
+            SetPowerupIcon();
         }
 
         private void SetPositionText()
@@ -35,6 +42,39 @@ namespace HUD
                     break;
                 default:
                     positionText.text = player.Position.ToString() + "th";
+                    break;
+            }
+        }
+
+        private void SetLapText()
+        {
+            lapText.text = player.lapNumber + "/3";
+        }
+
+        private void SetPowerupIcon()
+        {
+            switch (playerPowerupContainer.currentPowerup)
+            {
+                case "Speed Boost":
+                    currentPowerupIcon.sprite = powerupIcons[0];
+                    break;
+                case "Ball Projectile":
+                    currentPowerupIcon.sprite = powerupIcons[1];
+                    break;
+                case "Crystal Trap":
+                    currentPowerupIcon.sprite = powerupIcons[2];
+                    break;
+                case "Bone Trap":
+                    currentPowerupIcon.sprite = powerupIcons[3];
+                    break;
+                case "Mind's Eye":
+                    currentPowerupIcon.sprite = powerupIcons[4];
+                    break;
+                case "Catnip":
+                    currentPowerupIcon.sprite = powerupIcons[5];
+                    break;
+                default:
+                    currentPowerupIcon.sprite = powerupIcons[6];
                     break;
             }
         }
