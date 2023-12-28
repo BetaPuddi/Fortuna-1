18e2373c16b892643be1ea3c2d03059a572d0c10
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2023-12-28 11:37:32 +0000

    Keenan: Make pause screen
    Created the prefab PauseScreen.
    Made the pause screen appear when the pause button is pressed.

---
+++ Assets/TestScenes/Keenan/Assets/PauseScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    bool paused;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject pauseScreen;
+
+    //all input action stuff
+    private InputActionAsset inputActionAsset;
+
+    void Start()
+    {
+        paused = false;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        pauseScreen.SetActive(paused);
+    }
+
+    public void MainMenuButton()
+    {
+        paused = false;
+        SceneManager.LoadScene(0);
+    }
+    
+    public void ReplayButton()
+    {
+        paused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Reloads the scene
+    }
+
+    public void OnPause()
+    {
+        paused = !paused;
+    }
+}
