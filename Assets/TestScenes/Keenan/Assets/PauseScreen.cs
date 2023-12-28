da90cadda35da7d54cee5bc404ebbf39340b63b9
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2023-12-28 12:43:57 +0000

    Keenan: Fix pause menu buttons
    Fixed the pause menu buttons and allowed them to be highlighted

--- Assets/TestScenes/Keenan/Assets/PauseScreen.cs
+++ Assets/TestScenes/Keenan/Assets/PauseScreen.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PauseScreen : MonoBehaviour
 {
     bool paused;
-    [SerializeField] GameObject player;
+    GameObject player;
     [SerializeField] GameObject pauseScreen;
+    GameObject defaultSelectedButton;
+    [SerializeField] GameObject restartButton;
+    [SerializeField] EventSystem eventSystem;
 
     //all input action stuff
     private InputActionAsset inputActionAsset;
 
     void Start()
     {
+        defaultSelectedButton = eventSystem.firstSelectedGameObject;
         paused = false;
         Time.timeScale = 1.0f;
         player = GameObject.FindWithTag("Player");
@@ -29,17 +34,20 @@ public class PauseScreen : MonoBehaviour
     public void MainMenuButton()
     {
         paused = false;
+        Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
     
-    public void ReplayButton()
+    public void RestartButton()
     {
         paused = false;
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Reloads the scene
     }
 
     public void OnPause()
     {
         paused = !paused;
+        eventSystem.SetSelectedGameObject(paused ? restartButton : defaultSelectedButton);
     }
 }
