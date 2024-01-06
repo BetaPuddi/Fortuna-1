166b0ba0dd1dcfc69523659a01f511d880b7ccf0
Reece <18008512+BetaPuddi@users.noreply.github.com>
2024-01-06 13:05:43 +0000

    Added Win/Lose Assets and Scripting

--- Assets/Scripts/FinishScreen.cs
+++ Assets/Scripts/FinishScreen.cs
@@ -7,7 +7,7 @@ public class FinishScreen : MonoBehaviour
 {
     public GameObject player;
     private CartLap _lapTracker;
-    public GameObject endScreen;
+    public GameObject endScreen, winIcon, loseIcon;
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
@@ -17,7 +17,18 @@ public class FinishScreen : MonoBehaviour
     {
         if (_lapTracker.lapNumber == 4)
         {
-            endScreen.SetActive(true); 
+            endScreen.SetActive(true);
+            switch (player.GetComponent<CartLap>().Position)
+            {
+                case 1:
+                    winIcon.SetActive(true);
+                    loseIcon.SetActive(false);
+                    break;
+                default:
+                    loseIcon.SetActive(true);
+                    winIcon.SetActive(false);
+                    break;
+            }
         }
     }
     public void MainMenuButton()
